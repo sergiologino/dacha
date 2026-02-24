6bd49c284dddb71bdd087c72189585608713a326
savki <savkin@altacod.ru>
2026-02-24 19:55:12 +0300

    fix before to after in analytics again v2

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -8,7 +8,7 @@ export function YandexMetrika() {
 
   return (
     <>
-      <Script id="yandex-metrika" strategy="beforeInteractive">
+      <Script id="yandex-metrika" strategy="afterInteractive">
         {`
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
