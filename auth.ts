db5c8f5f00624a9c29a86afef991cc2dff87c87c
savki <savkin@altacod.ru>
2026-02-27 18:47:11 +0300

    fix bug for types

--- auth.ts
+++ auth.ts
@@ -55,9 +55,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     async session({ session, token }) {
       if (session.user) {
         session.user.id = (token.sub ?? token.id) as string;
-        session.user.name = token.name ?? null;
-        session.user.email = token.email ?? null;
-        session.user.image = token.picture ?? null;
+        session.user.name = token.name ?? undefined;
+        session.user.email = token.email ?? undefined;
+        session.user.image = token.picture ?? undefined;
       }
       return session;
     },
