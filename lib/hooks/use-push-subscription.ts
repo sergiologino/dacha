83a29a9bb2d37879bd768c4c300e6231ef84cf28
savki <savkin@altacod.ru>
2026-03-01 00:34:55 +0300

    fix bug 2

--- lib/hooks/use-push-subscription.ts
+++ lib/hooks/use-push-subscription.ts
@@ -62,7 +62,7 @@ export function usePushSubscription() {
       const reg = await navigator.serviceWorker.ready;
       const sub = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicKey),
+        applicationServerKey: urlBase64ToUint8Array(publicKey) as BufferSource,
       });
       const res = await fetch("/api/push/subscribe", {
         method: "POST",
